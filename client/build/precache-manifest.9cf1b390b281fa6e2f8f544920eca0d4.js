2bb95ed6ef411e7b0100a6e2a01cac3fbe0ea0a9
Singh-csm <csm8107@gmail.com>
2023-12-21 09:10:51 +0530

    done

---
+++ client/build/precache-manifest.9cf1b390b281fa6e2f8f544920eca0d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4bfb8c3dc8cb69169eb800dceccbeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33963dd527257b18cc65",
+    "url": "/static/css/main.c700b4c3.chunk.css"
+  },
+  {
+    "revision": "ec5c36ad23c9c48c93c2",
+    "url": "/static/js/2.5c816d2f.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.5c816d2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33963dd527257b18cc65",
+    "url": "/static/js/main.5489ade5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8784cb962b0593b5efb51100b12ec2ed",
+    "url": "/static/media/showcase.8784cb96.jpg"
+  }
+]);
---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    // await mongoose.connect(db);
+    await mongoose.connect("mongodb+srv://singh-csm:GIUPM5681K@singh-csm.nmfw5jk.mongodb.net/test-tech-network", {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected...!");
+  } catch (err) {
+    console.log(err.message);
+    // exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
