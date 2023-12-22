9021bd5654de28a023b523b6bdbb2fcc099d4bf7
Singh-csm <csm8107@gmail.com>
2023-12-22 11:54:12 +0530

    added

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const connectDB = require("./config/db");
 const path = require("path");
 const { ppid } = require("process");
 const app = express();
+const cors = require("cors")
+app.use(cors())
 
 // connect database
 connectDB();
@@ -19,15 +21,23 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static("build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "build", "index.html"));
+//   });
+// }
+
+app.use(express.static(path.join(__dirname, "./build")));
 
 const PORT = process.env.PORT || 5000;
 
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.get("*", (req, res) => {
+  const file = path.join(__dirname, "./build/index.html");
+  res.sendFile(file);
+});
